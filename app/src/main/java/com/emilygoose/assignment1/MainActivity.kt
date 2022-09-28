3c9b56bd8fb7388d026780910c9cf1d5c03edbb5
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-09-28 15:58:24 -0400

    Fragment switching to DeliveryFragment works

--- app/src/main/java/com/emilygoose/assignment1/MainActivity.kt
+++ app/src/main/java/com/emilygoose/assignment1/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     // Activity-level variables
     private var price = ""
-
-    // Declare required fragments
-    private lateinit var pizzaFragment: PizzaFragment
+    private var specialInstructions = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,28 +26,56 @@ class MainActivity : AppCompatActivity() {
         priceLabel = findViewById(R.id.form_total)
         checkoutButton = findViewById(R.id.button_checkout)
 
+        // Tracks what stage of the form we're in
+        var pizzaStage = true
+
         // Create a fragment result listener for the price
         supportFragmentManager.setFragmentResultListener(
-            "PizzaFragment",
-            this
+            "PizzaFragment", this
         ) { _, bundle -> // We use a String here, but any type that can be put in a Bundle is supported
-            val result = bundle.getString("price")
-            if (result != null) {
-                price = result
-                priceLabel.text = result
+            val priceResult = bundle.getString("price")
+            val instructionResult = bundle.getString("instructions")
+            if (priceResult != null) {
+                price = priceResult
+                priceLabel.text = priceResult
+            }
+            if (instructionResult != null) {
+                specialInstructions = instructionResult
             }
         }
 
         // Create the fragment for the first page of the order form
         supportFragmentManager.commit {
             setReorderingAllowed(true)
-            add<PizzaFragment>(R.id.fragment_container_view)
+            replace<PizzaFragment>(R.id.fragment_container_view)
         }
         checkoutButton.text = getString(R.string.prompt_continue)
 
         // Create click listener on continue button
         checkoutButton.setOnClickListener {
+            if (pizzaStage) {
+                pizzaStage = false
 
+                // Create a fragment result listener for the delivery
+                supportFragmentManager.setFragmentResultListener(
+                    "PizzaFragment", this
+                ) { _, bundle -> // We use a String here, but any type that can be put in a Bundle is supported
+                    val result = bundle.getString("price")
+                    if (result != null) {
+                        price = result
+                        priceLabel.text = result
+                    }
+                }
+
+                // Swap out PizzaFragment for DeliveryFragment
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace<DeliveryFragment>(R.id.fragment_container_view)
+                }
+                checkoutButton.text = getString(R.string.prompt_checkout)
+            } else { // Move to next activity
+                TODO("Needs to open OrderSummaryActivity")
+            }
         }
 
     }
